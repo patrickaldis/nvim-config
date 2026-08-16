69386e12cebb22278536abc02459ba9c6c9ac1f6
Patrick Aldis <patricktaldis@gmail.com>
2026-08-16 23:29:05 +0100

    fix treesitter warnings

--- config-plug/src/lua/config-plug/treesitter.lua
+++ config-plug/src/lua/config-plug/treesitter.lua
@@ -1,9 +1,11 @@
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd("BufEnter", {
   callback = function(args)
-    local ok = pcall(vim.treesitter.start, args.buf)
-    if not ok then
-        print("No treesitter parser found")
-        return
+    if vim.bo[args.buf].buftype == "" then
+        local ok = pcall(vim.treesitter.start, args.buf)
+        if not ok then
+            print("No treesitter parser found")
+            return
+        end
     end
   end,
 })
